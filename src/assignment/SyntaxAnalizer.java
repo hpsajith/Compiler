eb7dd53ca05e9a5f4dcb05b66313afbd6a9d18fe
admin <admin@DESKTOP-8SF2N15>
2017-08-03 16:58:49 +0530

    Office Syntax Analyzer Continued

--- src/assignment/SyntaxAnalizer.java
+++ src/assignment/SyntaxAnalizer.java
@@ -12,71 +12,43 @@ public class SyntaxAnalizer {
 
     LexicalAnalizer lx = new LexicalAnalizer();
 
-    public void analizerSyntax(String inputString) {
-        // TODO Auto-generated constructor stub
-        String s = inputString;
-        lx.getInputString(s);
-        s = s.replaceAll("\\s+", "");
-        System.out.println(s);
+    boolean checkParanthesis = false;
+    boolean checkSyntax = false;
+    boolean checkOperator = false;
+    boolean checkAssign = false;
+    boolean checkBooleanOperator = false;
+
+    boolean status = false;
+
+    private boolean program(Token methoDeclr, Token statList) {
+        String c = null;
+        Token literal = new Token(Token.Type.IDENTIFIER, c);
+        Token assignOp = new Token(Token.Type.ASSIGNOP, c);
+        Token numaral = new Token(Token.Type.NUMERICS, c);
+        
+                status = statList(statList) || statList(literal,assignOp,numaral);
+        return false;
+    }
+
+    private boolean statList(Token expression) {
+        return false;
+    }
+    private boolean statList(Token literal,Token assignOp,Token numeric) {
+        status = expression(literal) || expression(literal,assignOp,numeric);
+        return false;
+    }
+    private boolean expression(Token expression) {
+        return false;
+    }
+    private boolean expression(Token literal,Token assignOp,Token numeric) {
+        
+        return false;
+    }
+
+
+    public boolean syntaxParse(Stack st) {
+
         String c = "";
-        Stack st = new Stack();
-        String token = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            token = String.valueOf(token + s.charAt(i));
-            if (lx.methodDeclSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.lparantheseSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.keywordSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.identifierSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.biOperatorSet.contains(token)) {
-                if (lx.incrementOpSet.contains(token + s.charAt(i + 1))) {
-                } else {
-                    System.out.println(token);
-                    st.push(token);
-                    token = "";
-                }
-            } else if (lx.numericalSet.contains(token)) {
-                if (lx.numericalSet.contains(token + s.charAt(i + 1))) {
-                } else {
-                    System.out.println(token);
-                    st.push(token);
-                    token = "";
-                }
-            } else if (lx.sybolSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.operatorSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.rparantheseSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.assignOpSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            } else if (lx.incrementOpSet.contains(token)) {
-                System.out.println(token);
-                st.push(token);
-                token = "";
-            }
-        }
-
         while (0 < st.size()) {
             c = st.peek().toString();
             if (lx.rparantheseSet.contains(c)) {
@@ -94,6 +66,7 @@ public class SyntaxAnalizer {
                 break;
             }
         }
+
         while (0 < st.size()) {
             c = st.peek().toString();
             if (lx.identifierSet.contains(c)) {
@@ -157,7 +130,8 @@ public class SyntaxAnalizer {
             } else {
                 break;
             }
-        }while (0 < st.size()) {
+        }
+        while (0 < st.size()) {
             c = st.peek().toString();
             if (lx.identifierSet.contains(c)) {
                 st.pop();
@@ -205,7 +179,7 @@ public class SyntaxAnalizer {
                 break;
             }
         }////////////////////
-         while (0 < st.size()) {
+        while (0 < st.size()) {
             c = st.peek().toString();
             if (lx.numericalSet.contains(c)) {
                 st.pop();
@@ -260,7 +234,8 @@ public class SyntaxAnalizer {
             } else {
                 break;
             }
-        }while (0 < st.size()) {
+        }
+        while (0 < st.size()) {
             c = st.peek().toString();
             if (lx.numericalSet.contains(c)) {
                 st.pop();
@@ -348,5 +323,73 @@ public class SyntaxAnalizer {
                 break;
             }
         }
+
+        return false;
+    }
+
+    public void analizerSyntax(String inputString) {
+        // TODO Auto-generated constructor stub
+        String s = inputString;
+        lx.getInputString(s);
+        s = s.replaceAll("\\s+", "");
+        System.out.println(s);
+        Stack st = new Stack();
+        String token = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            token = String.valueOf(token + s.charAt(i));
+            if (lx.methodDeclSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.lparantheseSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.keywordSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.identifierSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.biOperatorSet.contains(token)) {
+                if (lx.incrementOpSet.contains(token + s.charAt(i + 1))) {
+                } else {
+                    System.out.println(token);
+                    st.push(token);
+                    token = "";
+                }
+            } else if (lx.numericalSet.contains(token)) {
+                if (lx.numericalSet.contains(token + s.charAt(i + 1))) {
+                } else {
+                    System.out.println(token);
+                    st.push(token);
+                    token = "";
+                }
+            } else if (lx.sybolSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.operatorSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.rparantheseSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.assignOpSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            } else if (lx.incrementOpSet.contains(token)) {
+                System.out.println(token);
+                st.push(token);
+                token = "";
+            }
+        }
+        boolean status = syntaxParse(st);
     }
 }
